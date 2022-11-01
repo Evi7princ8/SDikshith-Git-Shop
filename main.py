a7e8ced6c4c67961a22a2f8d6bb1d07312c7984b
Evi7princ8 <Sdikshith7885@gmail.com>
2022-11-01 12:53:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 print("Hello SDikshith :D")
 
+=======
+print("Hello SDikshith :D")
+>>>>>>> Stashed changes
+=======
+print(‘Hello world :D’)
+>>>>>>> Stashed changes
